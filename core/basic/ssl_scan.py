893b47f50812ae2c51f8a5d37da915f8b12e23ca
Lucky <ptl.om.22@gmail.com>
2025-12-19 12:57:28 +0530

    Implement SSL scan function in ssl_scan.py
    Added SSL scan functionality to check SSL certificate validity and save expiration date.

--- core/basic/ssl_scan.py
+++ core/basic/ssl_scan.py
@@ -1,1 +1,15 @@
 
+import ssl, socket, datetime
+from colorama import Fore
+def scan(domain, save_path):
+    print(Fore.YELLOW + "[*] Checking SSL...")
+    try:
+        ctx = ssl.create_default_context()
+        with socket.create_connection((domain, 443), timeout=5) as s:
+            with ctx.wrap_socket(s, server_hostname=domain) as ss:
+                cert = ss.getpeercert()
+                with open(f"{save_path}/ssl_info.txt", "w") as f:
+                    f.write(str(cert['notAfter']))
+        print(Fore.GREEN + "[✓] SSL Valid")
+    except: pass
+      
