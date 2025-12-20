1bdf09c86010deafdd68706557232990e20ec2a7
Lucky <ptl.om.22@gmail.com>
2025-12-20 11:09:48 +0530

    Update subdomain.py

--- core/basic/subdomain.py
+++ core/basic/subdomain.py
@@ -2,7 +2,7 @@ import requests
 from colorama import Fore
 def enumerate(domain, save_path):
     try:
-        # NO TIMEOUT - Will wait forever
+        # Infinite Wait (No timeout)
         url = f"https://crt.sh/?q=%.{domain}&output=json"
         data = requests.get(url).json() 
         subs = set(e['name_value'] for e in data)
@@ -10,5 +10,5 @@ def enumerate(domain, save_path):
             for s in subs: f.write(s+"\n")
         print(Fore.GREEN + f"    > Found {len(subs)} subdomains.")
     except Exception as e:
-        print(Fore.RED + f"    > Failed: {e}")
+        print(Fore.RED + f"    > Subdomain Scan Failed: {e}")
 
