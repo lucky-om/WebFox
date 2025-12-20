32b36845bdd62b6a5582b93afd39c70048c1fb17
Lucky <ptl.om.22@gmail.com>
2025-12-20 11:02:28 +0530

    Update dns_scan.py

--- core/basic/dns_scan.py
+++ core/basic/dns_scan.py
@@ -1,12 +1,15 @@
 import dns.resolver
 from colorama import Fore
 def scan(domain, save_path):
-    print(Fore.YELLOW + "[*] DNS Enumeration...")
-    with open(f"{save_path}/dns_records.txt", "w") as f:
-        for t in ['A','MX','NS']:
-            try:
-                ans = dns.resolver.resolve(domain, t)
-                for r in ans: f.write(f"{t}: {r.to_text()}\n")
-            except: pass
+    print(Fore.GREEN + "    > DNS Records Saved.")
+    try:
+        with open(f"{save_path}/dns.txt", "w") as f:
+            for r in ['A', 'MX', 'NS', 'TXT']:
+                try:
+                    ans = dns.resolver.resolve(domain, r)
+                    for d in ans: f.write(f"{r}: {d}\n")
+                except: pass
+    except: pass
+
     print(Fore.GREEN + "[✓] DNS saved")
   
