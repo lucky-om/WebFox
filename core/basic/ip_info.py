64251300fd535ea00000b42ee782675fb5e07535
Lucky <ptl.om.22@gmail.com>
2025-12-20 11:08:09 +0530

    Update ip_info.py

--- core/basic/ip_info.py
+++ core/basic/ip_info.py
@@ -1,10 +1,15 @@
 import socket, requests
 from colorama import Fore
 def scan(domain, save_path):
-    print(Fore.YELLOW + "[*] IP Geolocation...")
     try:
         ip = socket.gethostbyname(domain)
         r = requests.get(f"http://ip-api.com/json/{ip}").json()
-        with open(f"{save_path}/ip_location.txt", "w") as f: f.write(str(r))
+        print(Fore.GREEN + f"    > IP   : {ip}")
+        print(Fore.GREEN + f"    > Cloud: {r.get('isp')}")
+        with open(f"{save_path}/ip_location.txt", "w") as f:
+            f.write(f"IP Address  : {ip}\n")
+            f.write(f"City        : {r.get('city')}\n")
+            f.write(f"Region      : {r.get('regionName')}\n")
+            f.write(f"Country     : {r.get('country')}\n")
+            f.write(f"ISP (Cloud) : {r.get('isp')}\n")
     except: pass
-      
