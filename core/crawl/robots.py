d209b004484820302090602a3d45eaae9e52998a
Lucky <ptl.om.22@gmail.com>
2025-12-20 10:55:59 +0530

    Update robots.py

--- core/crawl/robots.py
+++ core/crawl/robots.py
@@ -2,6 +2,7 @@ import requests
 def scan(domain, save_path):
     try:
         r = requests.get(f"http://{domain}/robots.txt")
-        if r.status_code==200:
+        if r.status_code == 200:
             with open(f"{save_path}/robots.txt", "w") as f: f.write(r.text)
     except: pass
+
