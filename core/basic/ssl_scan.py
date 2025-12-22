f2224766d9024d24a49da1faab511280019765ac
Lucky <ptl.om.22@gmail.com>
2025-12-22 10:28:12 +0530

    Update ssl_scan.py

--- core/basic/ssl_scan.py
+++ core/basic/ssl_scan.py
@@ -3,7 +3,7 @@ from colorama import Fore
 def scan(domain, save_path):
     try:
         ctx = ssl.create_default_context()
-        with socket.create_connection((domain, 443)) as s:
+        with socket.create_connection((domain, 443), timeout=10) as s:
             with ctx.wrap_socket(s, server_hostname=domain) as ss:
                 c = ss.getpeercert()
                 issuer = dict(x[0] for x in c['issuer']).get('organizationName', 'Unknown')
@@ -11,4 +11,3 @@ def scan(domain, save_path):
                 with open(f"{save_path}/ssl_info.txt", "w") as f:
                     f.write(f"Issuer: {issuer}\nExpiry: {c['notAfter']}")
     except: pass
-
