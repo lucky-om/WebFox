666bd782fc6d7fa5f0eba1f39f7b3c91dab06331
Lucky <ptl.om.22@gmail.com>
2025-12-22 13:43:54 +0530

    Update ssl_scan.py

--- core/basic/ssl_scan.py
+++ core/basic/ssl_scan.py
@@ -1,13 +1,21 @@
 import ssl, socket
 from colorama import Fore
+
 def scan(domain, save_path):
     try:
         ctx = ssl.create_default_context()
-        with socket.create_connection((domain, 443), timeout=10) as s:
+        # Increased Timeout
+        with socket.create_connection((domain, 443), timeout=30) as s:
             with ctx.wrap_socket(s, server_hostname=domain) as ss:
                 c = ss.getpeercert()
                 issuer = dict(x[0] for x in c['issuer']).get('organizationName', 'Unknown')
-                print(Fore.GREEN + f"    > SSL  : Issued by {issuer}")
+                sans = [i[1] for i in c.get('subjectAltName', []) if i[0] == 'DNS']
+                
+                print(Fore.GREEN + f"    > Issuer: {issuer}")
+                print(Fore.CYAN + f"    > SANs: Found {len(sans)} linked domains")
+                
                 with open(f"{save_path}/ssl_info.txt", "w") as f:
-                    f.write(f"Issuer: {issuer}\nExpiry: {c['notAfter']}")
+                    f.write(f"Issuer: {issuer}\nExpiry: {c['notAfter']}\n\n[SANs]\n")
+                    f.write("\n".join(sans))
     except: pass
+
