0cf9168555dca986ad01e908d93ed322a7d0002a
Lucky <ptl.om.22@gmail.com>
2025-12-22 16:13:45 +0530

    Create dos_check.py

---
+++ core/basic/dos_check.py
@@ -0,0 +1,18 @@
+import socket
+from colorama import Fore
+def scan(domain, save_path):
+    print(Fore.YELLOW + "[*] Checking DoS Vulnerability...")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        s.connect((domain, 80))
+        s.send(f"GET / HTTP/1.1\r\nHost: {domain}\r\n".encode('utf-8'))
+        try:
+            s.recv(1024)
+            res = "Secure"
+        except socket.timeout:
+            res = "VULNERABLE (Slowloris)"
+            print(Fore.RED + f"    [!] {res}")
+        s.close()
+        with open(f"{save_path}/dos_vuln.txt", "w") as f: f.write(res)
+    except: pass
