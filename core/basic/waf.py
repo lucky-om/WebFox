dee4e559410a82d645732bb8ad4aff5ef875752c
Lucky <ptl.om.22@gmail.com>
2025-12-22 10:27:31 +0530

    Update waf.py

--- core/basic/waf.py
+++ core/basic/waf.py
@@ -3,11 +3,10 @@ from colorama import Fore
 def scan(domain, save_path):
     sigs = {"Cloudflare":"cf-ray", "Akamai":"akamai", "AWS":"awselb", "Imperva":"incap-ses", "F5":"bigip", "Google":"google-cloud-armor"}
     try:
-        r = requests.get(f"http://{domain}")
+        r = requests.get(f"http://{domain}", timeout=10)
         headers = str(r.headers).lower()
         found = [n for n,k in sigs.items() if k in headers]
         res = ", ".join(found) if found else "None Detected"
         print(Fore.GREEN + f"    > WAF  : {res}")
         with open(f"{save_path}/waf.txt", "w") as f: f.write(res)
     except: pass
-
